3d2e6e09486a3b17250c8afefb0e0b38eee248d5
ivnlsv <ivan.losev@denuo.legal>
2025-01-28 11:32:15 +0300

    Update

--- script.js
+++ script.js
@@ -1,15 +1,39 @@
-class First {
-    hello() {
-        console.log("Привет, я метод родителя!");
-    }
+const now = new Date();
+
+let greeting;
+const hours = now.getHours();
+if (hours < 6) {
+  greeting = "Доброй ночи";
+} else if (hours < 12) {
+  greeting = "Доброе утро";
+} else if (hours < 18) {
+  greeting = "Добрый день";
+} else {
+  greeting = "Добрый вечер";
 }
 
-class Second extends First {
-    hello() {
-        super.hello(); 
-        console.log("А я наследуемый метод!");
-    }
-}
+const daysOfWeek = [
+  "Воскресенье",
+  "Понедельник",
+  "Вторник",
+  "Среда",
+  "Четверг",
+  "Пятница",
+  "Суббота",
+];
+const dayOfWeek = daysOfWeek[now.getDay()];
 
-const secondInstance = new Second();
-secondInstance.hello();
+const currentTime = now.toLocaleTimeString("en-US");
+
+const nextYear = new Date(now.getFullYear() + 1, 0, 1); // 1 января следующего года
+const diffTime = nextYear - now;
+const daysUntilNewYear = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+
+document.getElementById("greeting").innerText = greeting;
+document.getElementById("today").innerText = `Сегодня: ${dayOfWeek}`;
+document.getElementById(
+  "currentTime"
+).innerText = `Текущее время: ${currentTime}`;
+document.getElementById(
+  "daysUntilNewYear"
+).innerText = `До нового года осталось ${daysUntilNewYear} дней`;
