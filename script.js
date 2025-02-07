5815340c70c33e992e917e48104646bf7888d3af
ivnlsv <ivan.losev@denuo.legal>
2025-02-07 15:13:29 +0300

    updated

--- script.js
+++ script.js
@@ -1,45 +1,47 @@
 // Функция для получения данных из файла db.json
-async function getData() {
-  try {
-      const response = await fetch('db.json'); // файл db.json должен находиться в корне проекта
-      if (!response.ok) {
-          throw new Error('Network response was not ok ' + response.statusText);
-      }
-      const data = await response.json();
-      return data;
-  } catch (error) {
-      console.error('Ошибка при получении данных:', error);
-  }
+const getData = () => {
+  return fetch('db.json') // файл db.json должен находиться в корне проекта
+      .then(response => {
+          if (!response.ok) {
+              throw new Error('Network response was not ok: ' + response.statusText);
+          }
+          return response.json();
+      })
+      .catch(error => {
+          console.error('Ошибка при получении данных:', error);
+      });
 }
 
 // Функция для отправки данных на указанный URL
-async function sendData(data) {
-  try {
-      const response = await fetch('https://jsonplaceholder.typicode.com/posts', {
-          method: 'POST',
-          headers: {
-              'Content-Type': 'application/json',
-          },
-          body: JSON.stringify(data),
-      });
-
+const sendData = (data) => {
+  return fetch('https://jsonplaceholder.typicode.com/posts', {
+      method: 'POST',
+      headers: {
+          'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+  })
+  .then(response => {
       if (!response.ok) {
-          throw new Error('Network response was not ok ' + response.statusText);
+          throw new Error('Network response was not ok: ' + response.statusText);
       }
-
-      const result = await response.json();
+      return response.json();
+  })
+  .then(result => {
       console.log('Данные успешно отправлены:', result);
-  } catch (error) {
+  })
+  .catch(error => {
       console.error('Ошибка при отправке данных:', error);
-  }
+  });
 }
 
 // Функция, которая выполняется при загрузке страницы
-async function init() {
-  const data = await getData();
-  if (data) {
-      await sendData(data);
-  }
+const init = () => {
+  getData().then(data => {
+      if (data) {
+          sendData(data);
+      }
+  });
 }
 
 // Вызываем init() при загрузке страницы
