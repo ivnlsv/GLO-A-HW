31ffc64f22510487369c029f35b37092889db520
ivnlsv <ivan.losev@denuo.legal>
2025-02-10 10:28:16 +0300

    added

--- script.js
+++ script.js
@@ -1,31 +1,21 @@
-const getData = () => {
-  return fetch('db.json') 
-      .then(response => response.json())
-      .catch(error => {
-          console.error('Ошибка при получении данных:', error);
-      });
-}
-const sendData = (data) => {
-  return fetch('https://jsonplaceholder.typicode.com/posts', {
-      method: 'POST',
-      headers: {
-          'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(data),
-  })
-  .then(response => response.json())
-  .then(result => {
-      console.log('Данные отправлены:', result);
-  })
-  .catch(error => {
-      console.error('Ошибка при отправке данных:', error);
-  });
-}
-const init = () => {
-  getData().then(data => {
-      if (data) {
-          sendData(data);
-      }
-  });
-}
-w.onload = init;
+let xhr = new XMLHttpRequest();
+xhr.open("POST", "https://jsonplaceholder.typicode.com/posts", true);
+xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+
+const data = {
+    title: "Lesson 26",
+    body: "XMLHttpRequest",
+    userId: 1
+};
+
+xhr.onreadystatechange = function() {
+    if (xhr.readyState === XMLHttpRequest.DONE) {
+        if (xhr.status === 201) {
+            console.log("Данные отправлены:", JSON.parse(xhr.responseText));
+        } else {
+            console.error("Ошибка при отправке данных:", xhr.statusText);
+        }
+    }
+};
+
+xhr.send(JSON.stringify(data));
